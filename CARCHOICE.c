d990a251523aae0c3fd1088444bd9382e180273f
Pranjal Agarwal <98935622+Pranjal360Agarwal@users.noreply.github.com>
2022-05-03 10:05:15 +0530

    Add files via upload

---
+++ CARCHOICE.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int t,a,b,c,d,r,s;
+   scanf("%d",&t);
+   while(t--)
+   {
+       scanf("%d %d %d %d",&a,&b,&c,&d);
+       r=a*d;
+       s=b*c;;
+       if(r>s)
+       printf("-1\n");
+       else if(s==r)
+       printf("0\n");
+       else
+       printf("1\n");
+   }
+    return 0;
+}
